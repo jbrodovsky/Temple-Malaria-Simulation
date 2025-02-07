0885c1f3ff11f64230bc0d5b58c2185d06fd90ab
James Brodovsky <james.brodovsky@gmail.com>
2025-02-07 12:56:16 -0500

    Added lz4 and openssl to dependencies

--- owlsnest_builder.sh
+++ owlsnest_builder.sh
@@ -14,13 +14,13 @@ module load gcc/12.2.0
 module load cmake/3.22.1
 module load git/2.34.1
 # installing lz4 for libpqxx support
-export CFLAGS="-std=c99"
-export CXXFLAGS="-std=c++11"
-./vcpkg install lz4
+#export CFLAGS="-std=c99"
+#export CXXFLAGS="-std=c++11"
+#vcpkg install lz4
 # installing openssl
-export CFLAGS="-DOPENSSL_NO_ASM" 
-export CXXFLAGS="-DOPENSSL_NO_ASM" 
-./vcpkg install openssl 
+#export CFLAGS="-DOPENSSL_NO_ASM" 
+#export CXXFLAGS="-DOPENSSL_NO_ASM" 
+#vcpkg install openssl 
 # ./vcpkg install gsl yaml-cpp fmt date args CLI11 gtest catch easyloggingpp sqlite3 libpqxx
 # Build the project
 mkdir -p build 
